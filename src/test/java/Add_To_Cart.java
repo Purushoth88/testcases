304714f8fdd1dbdbf264c98d58c2c0df8a7850c9
rami saleem <icreativeapp@gmail.com>
2013-02-14 06:31:12 +0300

    update

--- src/test/java/Add_To_Cart.java
+++ src/test/java/Add_To_Cart.java
@@ -29,6 +29,24 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
+//import java.util.NoSuchElementException;
+//import java.util.concurrent.TimeUnit;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+//import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
 /**
  *
  * @author Rami Saleem
