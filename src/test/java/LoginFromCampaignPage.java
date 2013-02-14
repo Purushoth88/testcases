77c6a80392e3aca72a387cbe71eb114bb363dfda
rami saleem <icreativeapp@gmail.com>
2013-02-15 00:37:51 +0300

    updates

--- src/test/java/LoginFromCampaignPage.java
+++ src/test/java/LoginFromCampaignPage.java
@@ -35,7 +35,7 @@ import org.openqa.selenium.interactions.Actions;
  * @author Rami Saleem
  */
 @Listeners({SauceOnDemandTestListener.class})
-public class Login_From_Campaign_Page implements SauceOnDemandSessionIdProvider, SauceOnDemandAuthenticationProvider {
+public class LoginFromCampaignPage implements SauceOnDemandSessionIdProvider, SauceOnDemandAuthenticationProvider {
 
     public SauceOnDemandAuthentication authentication;
 
@@ -95,7 +95,7 @@ public class Login_From_Campaign_Page implements SauceOnDemandSessionIdProvider,
     }
 
     @Test
-    public void LoginFromCampaignPage() throws Exception {
+    public void LoginFromCampaign() throws Exception {
 
 
         driver.get("http://markavip.com");
